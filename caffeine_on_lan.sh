945ef8fb021cbab921cde0ea18a35f3cf14551da
Diego Herranz <diegoherranz@diegoherranz.com>
2014-07-30 20:15:48 +0200

    First commit

---
+++ caffeine_on_lan.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+target=name #Replace with machine's name
+
+sleep 600 # Don't do anything unless power is stable
+
+while ! ping -c 1 ${target}
+do
+  wol ${target}
+  sleep 30
+done
+
+shutdown -h now
